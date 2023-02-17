b28f23c3e00caeffa3df0af5867f9e7d66b0864e
rihib <rihib@rihib.dev>
2023-02-17 12:03:00 +0000

    誤記を修正

--- Python/doctest_example.py
+++ Python/doctest_example.py
@@ -11,7 +11,7 @@ def hello(s: str) -> str:
         s (str): 任意の出力したい文字列。
 
     Returns:
-        s (str): 渡された文字列をそのまま返す。
+        str: 渡された文字列をそのまま返す。
 
     >>> from doctest_example import hello
     >>> hello("Hello, World!")
